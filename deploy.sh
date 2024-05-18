8c76ae526f41bd1ff3798340c82ef487ef265e55
Matt Crook <mattlcrook@icloud.com>
2024-05-19 10:44:19 +1200

    fix: file permissions not set correctly when deploying

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 hugo --environment production --cleanDestinationDir --minify --panicOnWarning
-rsync -rvz --delete public/ javelin:/volume1/web/matt/blog
+rsync -rvzhpg --delete --chmod=755 --chown=:http public/ javelin:/volume1/web/matt/blog
